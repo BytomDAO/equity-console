e1c342c2c90e777e7c82061a2fdcf9183489fed9
Yongfeng LI <wliyongfeng@gmail.com>
2018-03-06 15:30:45 +0800

    add LockedValue components

--- src/contracts/components/lockedValue.tsx
+++ src/contracts/components/lockedValue.tsx
@@ -1,15 +1,45 @@
 // external imports
 import * as React from 'react'
 import DocumentTitle from 'react-document-title'
+import Section from '../../app/components/section'
 
-const LockedValueDisplay = (props: {contractIds: string[], spentContractIds: string[] }) => {
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => {
+  return {
+    idList: state.templates.idList
+  }
+}
+
+const LockedValueDisplay = (props: {idList: string[]}) => {
+  const options = props.idList.slice(1).map(id => {
+    return <option key={id}>{id}</option>
+  })
+
+  const td = <td><button className="btn btn-primary btn-lg form-button">Unlock</button></td>
+
   return (
     <DocumentTitle title="Unlock Value">
       <div>
-        <span>Unlock Value</span>
+        <Section name="UTXO Params">
+          <div className={"form-group"}>
+            <label>UTXO ID:</label>
+            <input type="text" className="form-control string-input" />
+          </div>
+
+          <div className={"form-group"}>
+            <div className="input-group">
+              <div className="input-group-addon">Account</div>
+              <select className="form-control with-addon">
+                {options}
+              </select>
+            </div>
+          </div>
+        </Section>
+        <div><table><tbody><tr>{td}</tr></tbody></table></div>
       </div>
     </ DocumentTitle>
   )
 }
 
-export default LockedValueDisplay
+export default connect(mapStateToProps)(LockedValueDisplay)
