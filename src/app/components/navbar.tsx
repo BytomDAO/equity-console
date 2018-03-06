feef93c6b3235341f70691cb4f15c9e5bf9bfe83
Yongfeng LI <wliyongfeng@gmail.com>
2018-03-06 10:42:15 +0800

    add navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,38 @@
+// external imports
+import * as React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+// ivy imports
+import { prefixRoute } from '../../core'
+
+const logo = require('../../static/images/logo.svg')
+
+const mapStateToProps = () => {
+  const pathnames = window.location.pathname.split("/")
+  return { path: pathnames[pathnames.length - 1] }
+}
+
+const Navbar = (props: { path: string }) => {
+  return (
+    <nav className="navbar navbar-inverse navbar-static-top navbar-fixed-top">
+      <div className="container fixedcontainer">
+        <div className="navbar-header">
+          <a className="navbar-brand" href={prefixRoute('/')}>
+            <img src={logo} />
+          </a>
+        </div>
+        <ul className="nav navbar-nav navbar-right">
+          <li className={props.path === 'unlock' ? '' : 'active'} ><Link to={prefixRoute('/')}>Lock Value</Link></li>
+          <li className={props.path === 'unlock' ? 'active' : ''} ><Link to={prefixRoute('/unlock')}>Unlock Value</Link></li>
+          {/*<li className="divider-vertical"></li>*/}
+          {/*<li><a href="../dashboard" target="_blank">Dashboard</a></li>*/}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default connect(
+  mapStateToProps
+)(Navbar)
